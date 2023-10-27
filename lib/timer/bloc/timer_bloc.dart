a44eb27f39d760d2e990b98d3013607d631faa4d
jeongwongo <20123104a@gmail.com>
2023-10-27 17:55:44 +0900

    TimerBloc - define the dependency on our Ticker

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -1,16 +1,24 @@
 //consume the Ticker
 //bloc을 생성 = state, events, eventHandler를 생성
 
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_timer/ticker.dart';
 
 part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
   static const int _duration = 60;
 
-  TimerBloc() : super(TimerInitial(_duration)) {
-    // TODO: implement event handlers
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(TimerInitial(_duration)) {
+    //TODO : implement event handlers
   }
 }
